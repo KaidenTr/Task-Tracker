a015a006306dc040757d8f384c34a1448739c2d2
KaidenTr <101233202+KaidenTr@users.noreply.github.com>
2025-03-13 12:28:52 -0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskTracker
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task() { }
+
+        public void UpdateDetails(string newName, string newDescription, DateTime newDueDate)
+        {
+            Name = newName;
+            Description = newDescription;
+            DueDate = newDueDate;
+        }
+
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+        }
+    }
+}
